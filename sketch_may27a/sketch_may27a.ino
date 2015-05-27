e6575a84eaecc4ace8ecc88ce725098f4f3dfe48
otarkhan <osama.taha1994@gmail.com>
2015-05-27 19:54:41 +0300

    Update sketch_may27a.ino

--- sketch_may27a/sketch_may27a.ino
+++ sketch_may27a/sketch_may27a.ino
@@ -1,19 +1,14 @@
 #include <LiquidCrystal.h>
 
-// initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 
-//char i = '';
-
 void setup() {
   lcd.begin(16, 2);
-  
-  
 }
 
+
 void loop() {
- 
   for (int i=0 ; i < 5 ; i++);
   {
   lcd.setCursor(0,0);
