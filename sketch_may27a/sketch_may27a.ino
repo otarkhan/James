b259fc3b074476e11c5d88615169c186ad7d2322
Osama <osama.taha1994@gmail.com>
2015-05-27 19:38:46 +0300

    Arduino Code
    Arduino

---
+++ sketch_may27a/sketch_may27a.ino
@@ -0,0 +1,31 @@
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+
+//char i = '';
+
+void setup() {
+  lcd.begin(16, 2);
+  
+  
+}
+
+void loop() {
+ 
+  for (int i=0 ; i < 5 ; i++);
+  {
+  lcd.setCursor(0,0);
+  lcd.print("   Welcome to ");
+  lcd.setCursor(0, 1);
+  lcd.print("   Stockwood    ");
+  delay(2000);
+  lcd.setCursor(0, 0);
+  lcd.print("   Stockwood    ");
+  lcd.setCursor(0, 1);
+  lcd.print(" Vale Gun Club");
+  delay(2000);
+  }
+}
+
